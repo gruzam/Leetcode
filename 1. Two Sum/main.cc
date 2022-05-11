c0ec33798ea1f68874d2466a73aacf328121c66f
laikun jung <nodeblue@gmail.com>
2022-05-11 20:01:01 +0900

    without STL, Runtime: 7ms, Memory: 10.4MB

--- 1. Two Sum/main.cc
+++ 1. Two Sum/main.cc
@@ -5,19 +5,51 @@
 using namespace std;
 
 class Solution {
+	static const int MAX_RECORD = 10000;
+	static const int MAX_TABLE = MAX_RECORD * 3 + 7;
+    
+	int tbl_[MAX_TABLE];
+
+	void push(int Number, int Index) {
+		int h = (Number + 1000000000) % MAX_TABLE;
+		for (int i = 0; i < MAX_TABLE; i++) {
+			int index = tbl_[h];
+			if (index == -1 || index == MAX_RECORD) {
+				tbl_[h] = Index;
+				break;
+			}
+			h = (h + 1) % MAX_TABLE;
+		}
+	}
+
+	int find(int Number, const vector<int>& nums) {
+		int h = (Number + 1000000000) % MAX_TABLE;
+		for (int i = 0; i < MAX_TABLE; i++) {
+			int index = tbl_[h];
+			if (index == -1) return -1;
+
+			if (nums[index] == Number) {
+				return index;
+			}
+			h = (h + 1) % MAX_TABLE;
+		}
+		return -1;
+	}
+
 public:
+	Solution() {
+		for (int i = 0; i < MAX_TABLE; i++) tbl_[i] = -1;
+	}
+
 	vector<int> twoSum(vector<int>& nums, int target) {
-		map<int, int> hash;
-
-		int temp;
 		for (int i = 0; i < nums.size(); i++) {
-			temp = target - nums[i];
-			auto iter = hash.find(temp);
+			int temp = target - nums[i];
+			int index = find(temp, nums);
 
-			if (iter != hash.end())
-				return { i, iter->second };
-
-			hash.insert({ nums[i], i });
+			if (index != -1 && nums[index] == temp) {
+                return { index, i };
+			}
+			push(nums[i], i);
 		}
 		return { 0, 0 };
 	}
